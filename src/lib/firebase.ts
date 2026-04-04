ebaed8d637ecc4aab6601ae3aa63cc3cfed4e6b9
IgorFPonchielli <igor.ponchielli@gmail.com>
2026-04-04 11:16:15 -0300

    ci: configuração de auto-deploy com variáveis de ambiente

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,9 +8,9 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "983881062008",
-  appId: "1:983881062008:web:ffb7dafa38d061fe367f8d",
-  measurementId: "G-5Q5KQC5QMR"
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
