f65d1d5a55f60c39ea370c0d90eb58b71b0c313e
IgorFPonchielli <igor.ponchielli@gmail.com>
2026-04-05 19:54:36 -0300

    update localhost route

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -1,9 +1,6 @@
 export function generateStaticParams() {
   return [
-    { slug: "p" }, 
-    { slug: "igor-teste-slug" }, 
-    { slug: "igor-personal-trainer-unique" },
-    { slug: "test-loop-user-123" },
+    { slug: "p" },
     { slug: "igorponchielli" }
   ];
 }
